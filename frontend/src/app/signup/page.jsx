ecedacd14b26fb89120a10174d0155ac4216a9c5
Abheeshta-P <softwarecheck01+github@gmail.com>
2024-10-08 22:33:17 +0530

    bug fixes

--- frontend/src/app/signup/page.jsx
+++ frontend/src/app/signup/page.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function SignupUserTypeSelection() {
   return (
     <Container className={'flex justify-center items-center flex-col'}>
-      <h1 className={'mb-14 text-3xl md:text-4xl text-[#03089a] font-semibold'}>Select your role</h1>
+      <h1 className={'mb-14 text-3xl md:text-4xl text-zinc-900 font-semibold'}>Select your role</h1>
       <div className={'flex w-full flex-col md:flex-row gap-16 justify-center items-center'}>
       <Card title={'Event creator'} description={'The one who wants to manage events by using services of platform'} photo={'../../../cardImages/event-creator.png'}/>
       <Card title={'Service provider'} description={'The one who provides services to event creators through our platform'} photo={'../../../cardImages/service-provider.webp'}/>
--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Card({children,photo,title,description,className=''}) {
   return (
     <div className={`flex flex-col shadow-xl border border-black/10 rounded-lg w-[350px] md:w-[400px] h-[300px] md:h-[400px] justify-between p-5 ${className}`}>
-      <div className={'border border-black w-full h-[150px] md:h-[250px]'}>
-        <img src={photo} alt={title} className={'object-fit w-full h-full'}/>
+      <div className={'border border-black/30 w-full h-[150px] md:h-[250px]'}>
+        <img src={photo} alt={title} className={'object-cover w-full h-full'}/>
       </div>
       <div className={'flex flex-col gap-4'}>
         <h1 className={'font-semibold text-zinc-900'}>{title}</h1>
