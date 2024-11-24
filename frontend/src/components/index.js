206a95cd38615cd1ffcde72b74e7a0d35a56ac54
Abheeshta-P <softwarecheck01+github@gmail.com>
2024-11-24 11:20:51 +0530

    chnages

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,26 +1,49 @@
-import Header from "./Header/Header";
-import Footer from "./Footer/Footer";
-import Logo from "./Logo";
-import Button from "./Button";
-import Container from "./Container/Container";
-import Loading from "./Loading";
-import Input from "./Input";
-import Card from "./Card";
-import EventsCard from "./EventsCard";
-import Select from "./Select";
-import ProfileSection from "./Dashboard/ProfileCommon";
-import ServicesCard from "./ServicesCard";
+import Header from "./Header/Header.jsx";
+import Footer from "./Footer/Footer.jsx";
+import Logo from "./Logo.jsx";
+import Button from "./Button.jsx";
+import Container from "./Container/Container.jsx";
+import Loading from "./Loading.jsx";
+import Input from "./Input.jsx";
+import Card from "./Card.jsx";
+import EventsCard from "./EventsCard.jsx";
+import Select from "./Select.jsx";
+import ProfileSection from "./Dashboard/ProfileCommon.jsx";
+import ServicesCard from "./ServicesCard.jsx";
 import ServicesList from "./ServiceListScroll.jsx";
-import DetailedEventDisplayer from "./Dashboard/DetailedEvent";
-import ParticipantList from "./ParticipantsList";
-import TodoList from "./Todo/TodoList";
-import GalleryCard from "./GalleryCard";
+import DetailedEventDisplayer from "./Dashboard/DetailedEvent.jsx";
+import ParticipantList from "./ParticipantsList.jsx";
+import TodoList from "./Todo/TodoList.jsx";
+import GalleryCard from "./GalleryCard.jsx";
 
-import HomePage from "./Home/HomePage";
+import HomePage from "./Home/HomePage.jsx";
 
-import LayoutRender from "./LayoutRender";
-import ClientProvider from "./ClientProvider";
-import DashboardLayout from "./Dashboard/DashboardLayout";
-import HeaderServiceProviderDashboard from "./Dashboard/HeaderServiceProviderDashboard";
+import LayoutRender from "./LayoutRender.jsx";
+import ClientProvider from "./ClientProvider.jsx";
+import DashboardLayout from "./Dashboard/DashboardLayout.jsx";
+import HeaderServiceProviderDashboard from "./Dashboard/HeaderServiceProviderDashboard.jsx";
 
-export {Header,Footer,LayoutRender,ParticipantList,Logo,Button,HomePage,Container,Loading,Input,Card,Select,ProfileSection,EventsCard,ServicesCard,DetailedEventDisplayer,ServicesList,TodoList,ClientProvider,DashboardLayout,HeaderServiceProviderDashboard,GalleryCard}
+export {
+  Header,
+  Footer,
+  LayoutRender,
+  ParticipantList,
+  Logo,
+  Button,
+  HomePage,
+  Container,
+  Loading,
+  Input,
+  Card,
+  Select,
+  ProfileSection,
+  EventsCard,
+  ServicesCard,
+  DetailedEventDisplayer,
+  ServicesList,
+  TodoList,
+  ClientProvider,
+  DashboardLayout,
+  HeaderServiceProviderDashboard,
+  GalleryCard
+};
